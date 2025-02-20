cf014d530337045ed09664953f46d6fe50ba6825
Francisco Suarez <fjsuarez@gmail.com>
2025-02-20 11:18:15 +0100

    Created get users endpoint

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 class Settings(BaseSettings):
     PORT: int
@@ -10,11 +13,27 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+cred = credentials.Certificate('credentials.json')
+print(settings.DATABASE_URL)
+firebase_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': settings.DATABASE_URL
+})
+db = firestore.client(app=firebase_app, database_id="users")
+users_ref = db.collection("users")
+docs = users_ref.stream()
+
 app = FastAPI()
 
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
+
+@app.get("/")
+async def get_users():
+    users = []
+    for doc in docs:
+        users.append(doc.to_dict())
+    return users
 
 if __name__ == "__main__":
     import uvicorn
